c599258e887fc261bb719b2725d5f8b762c2a23e
MohammedBaqer-544 <almousawi2002@gmail.com>
2020-06-13 19:06:00 +0300

    finish
    done

---
+++ محمد باقر/محمد باقر الموسوي-٥٤٤.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var str = "Hello, playground"
+ 
+let name = "محمد باقر الموسوي"
+
+var height = 173.4
+
+var age = 17
+
+var hobby1 = "الطبخ"
+
+var hobby2 = "مشاهدة مسلسلات"
+
+print ("my name is", name)
+
+print ("my age is", age )
+
+print ("my height is", height)
+
+print ("my hobbies are", hobby1, "و" ,hobby2)
